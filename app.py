da89bcbc254d50e0591c6637afcdd6bdcc7f71d5
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2021-08-02 19:09:06 -0700

    implemented present

--- app.py
+++ app.py
@@ -18,54 +18,54 @@ if st.session_state['page'] == 'main':
     if title_cols[1].button("Click here to submit a recipe"):
         st.session_state['page'] = 'submit recipe'
 
-st.selectbox(
-    "Who's recipes are we using today?",
-    options = ['Erin', 'Landon', 'Both'],
-    index = 2,
-    key = 'user'
-)
+    st.selectbox(
+        "Who's recipes are we using today?",
+        options = ['Erin', 'Landon', 'Both'],
+        index = 2,
+        key = 'user'
+    )
 
-#in future, we want to be able to request a certain amount of each type
+    #in future, we want to be able to request a certain amount of each type
 
-n_recipes = int(st.number_input(
-    'How many recipes are we making?',
-    min_value = 1, max_value = None,
-))
+    n_recipes = int(st.number_input(
+        'How many recipes are we making?',
+        min_value = 1, max_value = None,
+    ))
 
-st.session_state['request'] = n_recipes
+    st.session_state['request'] = n_recipes
 
-### Read in relevant cookbook
-#TODO: this should be read from the cookbook database
+    ### Read in relevant cookbook
+    #TODO: this should be read from the cookbook database
 
-cookbook = random_cookbook()
+    cookbook = random_cookbook()
 
-#TODO: Create Recipe List
+    #TODO: Create Recipe List
 
-recipe_list = pick_recipes_randomly(cookbook, n_recipes)
+    recipe_list = pick_recipes_randomly(cookbook, n_recipes)
 
-#TODO: Determine Shopping List
+    #TODO: Determine Shopping List
 
-shopping_list = create_shopping_list(recipe_list)
+    shopping_list = create_shopping_list(recipe_list)
 
-#TODO: Create Meal Plan 
+    #TODO: Create Meal Plan 
 
-meal_plan = {'Recipes': recipe_list, 'Shopping List': shopping_list}
+    meal_plan = {'Recipes': recipe_list, 'Shopping List': shopping_list}
 
-#TODO: Serve Meal plan
+    #TODO: Serve Meal plan
 
-st.header("Heres what you'll be cooking this week:")
-st.write([str(recipe) for recipe in meal_plan['Recipes']])
+    st.header("Heres what you'll be cooking this week:")
+    st.write([str(recipe) for recipe in meal_plan['Recipes']])
 
-st.header("And here's your shopping list:")
+    st.header("And here's your shopping list:")
 
-shopping_df = pd.DataFrame([{'Ingredient': ingredient.element.name, 'Amount': ingredient.amount.value, 'Unit': str(ingredient.amount.unit)} for ingredient in meal_plan['Shopping List'].values()])
-print(shopping_df)
-st.write(shopping_df)
-#TODO: Allow for editing of plan
+    shopping_df = pd.DataFrame([{'Ingredient': ingredient.element.name, 'Amount': ingredient.present(), 'Unit': str(ingredient.amount.unit)} for ingredient in meal_plan['Shopping List'].values()])
 
-#TODO: and Regenerating 
+    st.write(shopping_df)
+    #TODO: Allow for editing of plan
 
-#TODO: Export
+    #TODO: and Regenerating 
+
+    #TODO: Export
 
 if st.session_state['page'] == 'submit':
     #TODO: how does a user submit a recipe?
--- src/cheffrey.py
+++ src/cheffrey.py
@@ -18,31 +18,104 @@ Generate the menu
 """
 
 from random import sample, randint, choice
+from bisect import bisect
+from fractions import Fraction
+
 import src.sugarcube as sc
 
 class Ingredient(sc.Ingredient):
 
-    def __add__(self, other):
-        if self.element.name != other.element.name:
-            raise TypeError(f'Your trying to add ingredients that are different elements: {self.element.name} and {other.element.name}')
+    
 
-        return Ingredient(sc.Amount(self.amount.value + other.to(self.amount.unit).amount.value, self.amount.unit), self.element)
+    def present(self):
+        """
+        Returns a human readable amount: 9 Quarts -> 2 Gallons 1 Quart
+        It gets the highest unit where you're > 1, and then if necessary gives the remainder in terms of the next unit, rounded to one of the breakpoints
+        """
 
-    def scale_up(self):
         relevant_units = self.amount.unit.measure.units
 
-        invalid_unit_names = ['decaliter', 'hectoliter', 'kiloliter']
+        invalid_unit_names = [name for name in relevant_units.keys() if 'liter' in name]
 
-        unit_list = [(unit, self.to(unit).amount.value) for unit in relevant_units.values() if unit.name not in invalid_unit_names]
+        unit_list = [(unit, round(self.to(unit).amount.value, 3)) for unit in relevant_units.values() if unit.name not in invalid_unit_names]
         unit_list.sort(key=lambda x: x[1], reverse=True)
-        print(unit_list)
+
+        whole_list = [(unit, value) for unit, value in unit_list if value >= 1]
+
+        first = whole_list.pop()
+        first_unit = first[0]
+        first_value = int(first[1] - (first[1] % 1))
+        remainder = first[1] % 1
+
+        #Granularity down to the 1/8 of a unit
+        cutoffs = [i/8 for i in range(8)]
+        print(remainder)
+        remainder = cutoffs[bisect(cutoffs, remainder)]
+
+        if remainder > 0:
+            remainder = Fraction(remainder)
+            return f"{first_value} {remainder.numerator}/{remainder.denominator} {first_unit}s"
+        else:
+            return f"{first_value} {first_unit}s"
+
+
+        # Instead of giving another unit, we're just gonna round the first unit
+        # second = whole_list.pop()
+        # second_unit = second[0]
+        # second_value = sc.Amount(remainder, first_unit).to(second_unit).value
+
+
+        
+        
+
+
+
+        
+        
+
+        return unit_list
 
         #Start at the smallest value, if you can go up a unit and still be >2, then do it
         unit, value = unit_list.pop()
+
+        # relevant_units
+
+        # if self.unit.measure == sc.Mass:
+        #     pass
+
+        # elif self.unit.measure == sc.sc.Volume:
+        #     pass
+            
+        # else:
+        #     raise TypeError(f"Cannot scale up measure: {self.unit.measure}")
+
+        
+
+
         
 
 
         pass
+
+    def __add__(self, other):
+        if isinstance(other, Ingredient):
+            if self.element.name != other.element.name:
+                raise TypeError(f'Your trying to add ingredients that are different elements: {self.element.name} and {other.element.name}')
+
+            return Ingredient(sc.Amount(self.amount.value + other.to(self.amount.unit).amount.value, self.amount.unit), self.element)
+        elif isinstance(other, (int, float)):
+            return Ingredient(sc.Amount(self.amount.value + other, self.amount.unit), self.element)
+        else:
+            raise TypeError(f"Dont know how to add an ingredient and an object of type {type(other)}")
+
+    def __mul__(self, other):
+        if isinstance(other, (int, float)):
+            return Ingredient(sc.Amount(self.amount.value * other, self.amount.unit), self.element)
+        else:
+            raise TypeError(f"Dont know how to multiply an ingredient and an object of type {type(other)}")
+    
+    def __rmul__(self, other):
+        return self.__mul__(other)
 
 
 
@@ -99,8 +172,6 @@ def create_shopping_list(recipes):
     for recipe in recipes:
         for ingredient in recipe.ingredients:
             if ingredient.element.name in shopping_list.keys():
-                print(f'Existing: {shopping_list[ingredient.element.name]}')
-                print(f"New: {ingredient}")
                 shopping_list[ingredient.element.name] += ingredient
             else:
                 shopping_list[ingredient.element.name] = ingredient
