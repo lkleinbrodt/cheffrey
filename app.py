191c46ea4f2076745467a8ae74341615067cc64e
Landon Kleinbrodt <lkleinbrodt@gmail.com>
2022-09-17 18:02:10 -0700

    downloaded 100 NYT recipes

--- app.py
+++ app.py
@@ -2,30 +2,41 @@
 import streamlit as st
 from src.cheffrey import *
 import pandas as pd
+from math import ceil, floor
 import yaml
 import logging
 import itertools
-logging.basicConfig(level = logging.DEBUG, format = ' %(asctime)s - $(levelname)s - $(message)s', filemode = 'w')
+logging.basicConfig(level=logging.DEBUG,
+                    format=' %(asctime)s - $(levelname)s - $(message)s', filemode='w')
 logger = logging.getLogger(__name__)
 logger.debug('Start of program')
 
 
-### Callbacks and Session State Functions
+# Callbacks and Session State Functions
 
 def update_page(page_name):
     logger.info(f'Switching page to: {page_name}')
     st.session_state['page'] = page_name
 
+
 def add_tag():
     st.session_state['available_tags'] += [st.session_state['new_tag']]
 
+
 def submit_recipe(recipe):
     logger.info('Submitting recipe')
+
+    if len(recipe['Ingredients']) == 0:
+        st.warning('Make sure to enter ingredients')
+        st.stop()
+
     st.session_state['submitted_recipe'] = recipe
     st.session_state['page'] = 'edit recipe'
 
+
 def store_recipe(recipe):
-    add_to_recipe_file(recipe, overwrite = True) #we already checked if one exists
+    # we already checked if one exists
+    add_to_recipe_file(recipe, overwrite=True)
     st.session_state['master_recipes'][recipe['Title']] = recipe
     st.session_state['page'] = 'submit'
 
@@ -38,7 +49,7 @@ def download_recipe():
     recipe = scrape_recipe(url_to_download)
     recipe = parse_scraped_recipe(recipe)
     recipe['URL'] = url_to_download
-    #TODO: check to see if we already have that recipe
+
     if recipe['Title'] in st.session_state['master_recipes'].keys():
         existing_recipe = st.session_state['master_recipes'][recipe['Title']]
         st.write("We already have that recipe loaded, does this look right?")
@@ -47,32 +58,48 @@ def download_recipe():
         st.write(existing_recipe['Instructions'])
 
         st.button("Looks right to me!", on_click=update_page, args=('submit',))
-        st.button("Wrong, replace this recipe", on_click=submit_recipe, args = (recipe))
+        st.button("Wrong, replace this recipe",
+                  on_click=submit_recipe, args=(recipe))
     else:
         submit_recipe(recipe)
+
+
+def regen_recipe(i, cookbook, method):
+    if method == 'random':
+        new_recipe = pick_recipes_randomly(cookbook, 1)[0]
+    elif method == 'searched':
+        searched_value = st.session_state[f'search_{i}']
+        new_recipe = cookbook.recipes[searched_value]
+    else:
+        raise ValueError(f"Unrecognized method: {method}")
+    st.session_state['recipe_list'][i] = new_recipe
+
 
 def load_config():
     cfg = load_yaml('config')
     master_recipes = load_yaml('recipes')
     return cfg, master_recipes
 
+
 ##################
-#########App
+# App
 ##################
 
-### Initialize Session
-
+# Initialize Session
 cfg, original_master_recipes = load_config()
 
 if 'master_recipes' not in st.session_state.keys():
     st.session_state['master_recipes'] = original_master_recipes
 if 'page' not in st.session_state.keys():
     st.session_state['page'] = 'main'
+if 'recipe_list' not in st.session_state.keys():
+    st.session_state['recipe_list'] = []
 
 if 'available_tags' not in st.session_state.keys():
     base_tags = ['breakfast', 'lunch', 'dinner']
     try:
-        existing_tags = list(itertools.chain.from_iterable([st.session_state['master_recipes'][name]['Tags'] for name in st.session_state['master_recipes'].keys()]))
+        existing_tags = list(itertools.chain.from_iterable(
+            [st.session_state['master_recipes'][name]['Tags'] for name in st.session_state['master_recipes'].keys()]))
         tags = list(set(base_tags + existing_tags))
     except TypeError:
         tags = base_tags
@@ -81,16 +108,17 @@ if 'available_tags' not in st.session_state.keys():
 
 
 ###
-### Display:
+# Display:
 
 logger.info(f"Showing: {st.session_state['page']}")
 
 if st.session_state['page'] == 'main':
-    title_cols = st.columns([3,1])
+    title_cols = st.columns([3, 1])
 
     title_cols[0].title("Howdy! I'm Cheffrey")
 
-    title_cols[1].button("Click here to submit a recipe", on_click=update_page, args=('submit',))
+    title_cols[1].button("Click here to submit a recipe",
+                         on_click=update_page, args=('submit',))
 
     # st.selectbox(
     #     "Who's recipes are we using today?",
@@ -99,81 +127,133 @@ if st.session_state['page'] == 'main':
     #     key = 'user'
     # )
 
-    #in future, we want to be able to request a certain amount of each type
+    # in future, we want to be able to request a certain amount of each type
 
     n_recipes = int(st.number_input(
         'How many recipes are we making?',
-        min_value = 1, max_value = None,
+        min_value=1,
+        max_value=None,
+        value=4
     ))
 
     st.session_state['request'] = n_recipes
 
-    ### Read in relevant cookbook
-    #TODO: this should be read from the cookbook database
+    # Read in relevant cookbook
+    # TODO: this should be read from the cookbook database
 
-    cookbook = Cookbook('master', recipe_dict = st.session_state['master_recipes'])
+    cookbook = Cookbook(
+        'master',
+        recipe_dict=st.session_state['master_recipes']
+    )
 
-    #TODO: Create Recipe List
+    # TODO: Create Recipe List
 
-    recipe_list = pick_recipes_randomly(cookbook, n_recipes)
+    @st.cache(allow_output_mutation=True)
+    def pick_recipes(cookbook, n_recipes):
+        recipe_list = pick_recipes_randomly(
+            cookbook, n_recipes)
+        st.session_state['recipe_list'] = recipe_list
+        return recipe_list
 
-    #TODO: Determine Shopping List
+    pick_recipes(cookbook, n_recipes)
+
+    recipe_list = st.session_state['recipe_list']
+
+    # TODO: Determine Shopping List
 
     shopping_list = create_shopping_list(recipe_list)
 
-    #TODO: Create Meal Plan 
+    # TODO: Create Meal Plan
 
     meal_plan = {'Recipes': recipe_list, 'Shopping List': shopping_list}
 
-    #TODO: Serve Meal plan
+    # TODO: Serve Meal plan
 
     st.header("Heres what you'll be cooking this week:")
 
-    for recipe in meal_plan['Recipes']:
-        st.subheader(recipe['Title'])
-        st.write(f"Time: {recipe['Time']}")
-        st.write(f"Tags: {recipe['Tags']}")
-        st.image(recipe['Image'])
+    n_cols = min([len(meal_plan['Recipes']), 4])
+    n_rows = ceil(len(meal_plan['Recipes']) / n_cols)
 
-    st.header("And here's your shopping list:")
+    def make_grid(n_cols, n_rows):
+        grid = [0]*n_rows
+        for i in range(n_rows):
+            with st.container():
+                grid[i] = st.columns(n_cols)
+        return grid
 
-    st.write(shopping_list)
+    recipe_grid = make_grid(n_cols, n_rows)
 
-    #TODO: Allow for editing of plan
+    for i, recipe in enumerate(meal_plan['Recipes']):
+        row_idx = floor(i / n_cols)
+        col_idx = i % n_cols
+        with recipe_grid[row_idx][col_idx]:
+            st.subheader(recipe['Title'])
+            # st.write(f"Time: {recipe['Time']}")
+            # st.write(f"Tags: {recipe['Tags']}")
+            st.image(recipe['Image'])
 
-    #TODO: and Regenerating 
+            with st.expander(label='ingredients', expanded=False):
+                for r in recipe['Ingredients']:
+                    st.write(r)
 
-    #TODO: Export
+            st.button(
+                label='Pick Again', key=f'regen_{i}',
+                on_click=regen_recipe, args=(i, cookbook, 'random')
+            )
 
+            st.selectbox(
+                label='Search',
+                key=f'search_{i}',
+                options=cookbook.recipes.keys(),
+                on_change=regen_recipe,
+                args=(i, cookbook, 'searched')
+            )
 
-    
+    # TODO: Allow for editing of plan
+
+    # TODO: and Regenerating
+
+    # TODO: Export
+
+    html = create_meal_plan_html(meal_plan)
+    with open('./meal_plan.html', 'w') as f:
+        f.write(html)
+
+    st.download_button(
+        label='Generate Meal Plan',
+        data=html, file_name='meal_plan.html', mime='text/html'
+    )
+
 
 if st.session_state['page'] == 'submit':
-    t,b= st.columns([5,1])
+    t, b = st.columns([5, 1])
     t.title('Submit a New Recipe')
     b.button('Back to main page', on_click=update_page, args=('main',))
-    #TODO: how does a user submit a recipe?
-    url_to_download = st.text_input("Enter the URL of the recipe:", on_change=download_recipe, key = 'url_to_download')
+    # TODO: how does a user submit a recipe?
+    url_to_download = st.text_input(
+        "Enter the URL of the recipe:", on_change=download_recipe, key='url_to_download')
 
-    st.button('Click here to enter one manually:', on_click=update_page, args=('manual_submit',))
+    st.button('Click here to enter one manually:',
+              on_click=update_page, args=('manual_submit',))
 
 if st.session_state['page'] == 'edit recipe':
     recipe = st.session_state['submitted_recipe']
-    
+
     st.header("Here's your recipe:")
     st.title(recipe['Title'])
-    st.image(recipe['Image'])
+    if recipe['Image']:
+        st.image(recipe['Image'])
     st.header('Ingredients:')
-    st.write('\\n'.join([f for f in recipe['Ingredients']]))
+    st.write('\n'.join([f for f in recipe['Ingredients']]))
     st.header('Instructions:')
     st.write(recipe['Instructions'])
 
     st.header('Add Tags')
-    
+
     recipe_tags = st.multiselect(
         "Tags:",
         options=st.session_state['available_tags'],
-        default = None
+        default=None
     )
     st.text_input('Create new tag:', on_change=add_tag, key='new_tag')
 
@@ -182,40 +262,41 @@ if st.session_state['page'] == 'edit recipe':
     st.button('Submit Recipe:', on_click=store_recipe, args=(recipe,))
 
 
-if st.session_state['page'] == 'manually_submit':
-    st.warning('Sorry, not implemented yet!')
-    # with st.form(key = 'submit_recipe_form'):
-    #         n_max_ingredients = 50
-    #         ingredients = []
-    #         amounts = []
-    #         submit_cols = st.columns(3)
-    #         name = submit_cols[0].text_input("What's the name of the dish?")
-    #         instructions = submit_cols[0].text_input("Instructions:")
-    #         tags = submit_cols[0].text_input("Tags:")
-    #         #TODO: tags should be a list of existing tags, with the option of adding a new one
+if st.session_state['page'] == 'manual_submit':
+    st.title('Submitting recipe')
+    submit_cols = st.columns(2)
+    name = submit_cols[0].text_input("What's the name of the dish?")
+    instructions = submit_cols[1].text_input("Instructions:")
 
-    #         for i in range(n_max_ingredients):
-    #             submit_cols[0].text_input(f"Ingredient {i}", key = f"ingredient_{i}")
-    #             ingredients.append()
-    #             submit_cols[1].text_input(f"Amount:")
-            
-    #         submit = st.form_submit_button('Submit Recipe:')
+    n_ingredients = int(st.number_input(
+        'How many ingredients?', min_value=1, step=1, value=5))
 
-    #         if submit:
-    #             #TODO: parse and validate ingredients
-    #             ingredients = [ing for ing in ingredients if ing != '']
-    #             amounts = [amt for amt in amounts if amt != '']
-                
-    #             if len(ingredients) == 0:
-    #                 st.warning('Make sure to enter ingredients')
-    #                 st.stop()
-    #             if len(ingredients) == 0:
-    #                 st.warning('Make sure to enter amounts')
-    #                 st.stop()
-                
+    ingredients = []
 
-    #             #recipe_df = pd.DataFrame({"Ingredient" = ingredients})
-    #             #TODO: parse name, instructions, tags
-    #             #TODO: combine into one document
-    #             #TODO: store
+    for i in range(n_ingredients):
+        ingredients.append(st.text_input(
+            f"Ingredient {i+1}", key=f"ingredient_{i+1}"))
 
+    ingredients = [ing for ing in ingredients if ing != '']
+
+    recipe = {
+        'Title': name,
+        'Time': None,
+        'Yield': None,
+        'Ingredients': ingredients,
+        'Instructions': instructions,
+        'Image': None,
+        'URL': None,
+        'Tags': None
+    }
+
+    if recipe['Title'] in st.session_state['master_recipes'].keys():
+        existing_recipe = st.session_state['master_recipes'][recipe['Title']]
+        st.write("We already have that recipe loaded, does this look right?")
+        st.write(recipe['Title'])
+        st.write(existing_recipe['Ingredients'])
+        st.write(existing_recipe['Instructions'])
+
+        st.button("Looks right to me!", on_click=update_page, args=('submit',))
+
+    st.button('Submit and Review', on_click=submit_recipe, args=(recipe,))
--- src/cheffrey.py
+++ src/cheffrey.py
@@ -22,14 +22,18 @@ from bisect import bisect
 from fractions import Fraction
 import itertools
 
+from xhtml2pdf import pisa
 from streamlit.state.session_state import Value
 import src.sugarcube as sc
 import logging
 from recipe_scrapers import scrape_me as scrape_recipe
 import yaml
-logging.basicConfig(level = logging.DEBUG, format = ' %(asctime)s - $(levelname)s - $(message)s', filemode = 'w')
+import streamlit as st
+logging.basicConfig(level=logging.DEBUG,
+                    format=' %(asctime)s - $(levelname)s - $(message)s', filemode='w')
 logger = logging.getLogger(__name__)
 logger.debug('Start of program')
+
 
 def load_yaml(name):
     valid_names = ['config', 'recipes']
@@ -37,7 +41,9 @@ def load_yaml(name):
         with open(f"data/{name}.yaml", 'r') as f:
             return yaml.safe_load(f)
     else:
-        raise ValueError(f"Unrecognized name: {name}. Valid types are: {valid_names}")
+        raise ValueError(
+            f"Unrecognized name: {name}. Valid types are: {valid_names}")
+
 
 def parse_scraped_recipe(recipe):
     d = {
@@ -51,19 +57,23 @@ def parse_scraped_recipe(recipe):
     }
     return d
 
-def add_to_recipe_file(recipe, overwrite = False):
+
+def add_to_recipe_file(recipe, overwrite=False):
     all_recipes = load_yaml('recipes')
 
-    if (recipe['Title'] in all_recipes.keys()) & (overwrite==False):
-        raise FileExistsError(f"{recipe.title()} already found in the file, and overwrite set to false")
-    
+    if (recipe['Title'] in all_recipes.keys()) & (overwrite == False):
+        raise FileExistsError(
+            f"{recipe.title()} already found in the file, and overwrite set to false")
+
     all_recipes[recipe['Title']] = recipe
-    
+
     with open(f"data/recipes.yaml", 'w') as f:
         yaml.safe_dump(all_recipes, f)
 
-#### Yknow what. let's just build a simple version first
-### No combining ingredients, just picking meals and pasting the ingredients together
+# Yknow what. let's just build a simple version first
+# No combining ingredients, just picking meals and pasting the ingredients together
+
+
 class Ingredient(sc.Ingredient):
 
     def present(self):
@@ -74,9 +84,11 @@ class Ingredient(sc.Ingredient):
 
         relevant_units = self.amount.unit.measure.units
 
-        invalid_unit_names = [name for name in relevant_units.keys() if 'liter' in name]
+        invalid_unit_names = [
+            name for name in relevant_units.keys() if 'liter' in name]
 
-        unit_list = [(unit, round(self.to(unit).amount.value, 3)) for unit in relevant_units.values() if unit.name not in invalid_unit_names]
+        unit_list = [(unit, round(self.to(unit).amount.value, 3))
+                     for unit in relevant_units.values() if unit.name not in invalid_unit_names]
         unit_list.sort(key=lambda x: x[1], reverse=True)
 
         whole_list = [(unit, value) for unit, value in unit_list if value >= 1]
@@ -86,9 +98,9 @@ class Ingredient(sc.Ingredient):
         first_value = int(first[1] - (first[1] % 1))
         remainder = first[1] % 1
 
-        #Granularity down to the 1/8 of a unit
+        # Granularity down to the 1/8 of a unit
         cutoffs = [i/8 for i in range(9)]
-        
+
         try:
             remainder = cutoffs[bisect(cutoffs, remainder)]
         except IndexError as err:
@@ -97,7 +109,6 @@ class Ingredient(sc.Ingredient):
             logger.info(f"bisect: {bisect(cutoffs, remainder)}")
             logger.info(err.args)
             raise
-
 
         if remainder > 0:
             remainder = Fraction(remainder)
@@ -108,37 +119,41 @@ class Ingredient(sc.Ingredient):
     def __add__(self, other):
         if isinstance(other, Ingredient):
             if self.element.name != other.element.name:
-                raise TypeError(f'Your trying to add ingredients that are different elements: {self.element.name} and {other.element.name}')
+                raise TypeError(
+                    f'Your trying to add ingredients that are different elements: {self.element.name} and {other.element.name}')
 
             return Ingredient(sc.Amount(self.amount.value + other.to(self.amount.unit).amount.value, self.amount.unit), self.element)
 
         elif isinstance(other, (int, float)):
             if self.element.is_int:
-                assert (other % 1) == 0, f"{self.element.name} is a whole ingredient, can only add whole numbers"
+                assert (
+                    other % 1) == 0, f"{self.element.name} is a whole ingredient, can only add whole numbers"
             return Ingredient(sc.Amount(self.amount.value + other, self.amount.unit), self.element)
         else:
-            raise TypeError(f"Dont know how to add an ingredient and an object of type {type(other)}")
+            raise TypeError(
+                f"Dont know how to add an ingredient and an object of type {type(other)}")
 
     def __mul__(self, other):
         if isinstance(other, (int, float)):
-            #Can you multiply 1 egg by 1.5? or should that be illegal? It might blow up scaling :/
+            # Can you multiply 1 egg by 1.5? or should that be illegal? It might blow up scaling :/
             return Ingredient(sc.Amount(self.amount.value * other, self.amount.unit), self.element)
         else:
-            raise TypeError(f"Dont know how to multiply an ingredient and an object of type {type(other)}")
-    
+            raise TypeError(
+                f"Dont know how to multiply an ingredient and an object of type {type(other)}")
+
     def __rmul__(self, other):
         return self.__mul__(other)
 
     def _from_strings(amount_str, element_str):
         logger.info(amount_str)
         logger.info(element_str)
-        #Parse amount
+        # Parse amount
         amount_list = str.split(amount_str)
         value = float(amount_list[0])
         if (value % 1) == 0:
             value = int(value)
         unit_str = ' '.join(amount_list[1:])
-        
+
         matched = False
         for u in sc.Volume.units:
             if unit_str == u:
@@ -156,8 +171,9 @@ class Ingredient(sc.Ingredient):
             raise ValueError(f'Unrecognized unit: {unit_str}')
 
         if element_str not in known_elements.keys():
-            raise ValueError(f"Unrecognized element: {element_str}, you might need to add it to the master list")
-        
+            raise ValueError(
+                f"Unrecognized element: {element_str}, you might need to add it to the master list")
+
         element = known_elements[element_str]
 
         return Ingredient(sc.Amount(value, unit), element)
@@ -172,10 +188,13 @@ class Recipe(object):
     def __str__(self) -> str:
         return f"Recipe for {self.name}: {self.ingredients}"
 
+
 def yaml_to_recipe(recipe_name, recipe_dict):
     logger.info(recipe_dict)
-    ingredients = {ingredient_name: Ingredient._from_strings(amount_str, ingredient_name) for ingredient_name, amount_str  in recipe_dict['ingredients'].items()}
+    ingredients = {ingredient_name: Ingredient._from_strings(
+        amount_str, ingredient_name) for ingredient_name, amount_str in recipe_dict['ingredients'].items()}
     return Recipe(recipe_name, ingredients, recipe_dict['instructions'])
+
 
 class Cookbook(object):
     def __init__(self, title, recipe_dict):
@@ -184,35 +203,42 @@ class Cookbook(object):
         #self.recipes = {name: yaml_to_recipe(name, rec) for name, rec in recipe_dict.items()}
 
 
-### Populating Classes
+# Populating Classes
 
 def random_ingredient():
-    i = randint(1,10)
+    i = randint(1, 10)
 
     unit = sc.Volume.units[choice(list(sc.Volume.units.keys()))]
-    amount = sc.Amount(i, unit = unit)
+    amount = sc.Amount(i, unit=unit)
     sample_elements = [sc.Element(f'Ingredient_{i}') for i in range(20)]
     element = sample(sample_elements, 1)[0]
     return Ingredient(amount, element)
-        
+
+
 def random_cookbook():
-    
+
     instructions = 'Cook them up real nice'
-    
-    my_recipes = [Recipe(f'sammy_{i}', [random_ingredient() for i in range(6)], instructions) for i in range(50)]
+
+    my_recipes = [Recipe(f'sammy_{i}', [random_ingredient()
+                         for i in range(6)], instructions) for i in range(50)]
     my_cookbook = Cookbook("Lando's Cookbook")
     my_cookbook.recipes = my_recipes
 
     return my_cookbook
-# Recipes are entered into an excel template sheet, I will format it so that it can be read in and parsed to this 
+# Recipes are entered into an excel template sheet, I will format it so that it can be read in and parsed to this
 # we can always bulk process into json format for faster read times
 
+
+
 def pick_recipes_randomly(cookbook, n_recipes):
-    recipes = sample([recipe for recipe in cookbook.recipes.values()], k=n_recipes)
+    recipes = sample(
+        [recipe for recipe in cookbook.recipes.values()], k=n_recipes)
     return recipes
 
+
 def create_shopping_list(recipe_list):
-    ingredients = list(itertools.chain.from_iterable([rec['Ingredients'] for rec in recipe_list]))
+    ingredients = list(itertools.chain.from_iterable(
+        [rec['Ingredients'] for rec in recipe_list]))
     return ingredients
 # def create_shopping_list(recipes):
 #     shopping_list = {}
@@ -227,7 +253,100 @@ def create_shopping_list(recipe_list):
 
 #     return shopping_list
 
-#This should really be in a separate file:
+
+def css_style():
+    with open('./meal_plan_style.css', 'r') as f:
+        x = f.read()
+    return x
+
+
+def create_meal_plan_html(meal_plan):
+    shopping_list = meal_plan['Shopping List']
+    recipes = meal_plan['Recipes']
+
+    # <link rel="stylesheet" type="text/css" href="example.css" media=”screen” />
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+    <style type="text/css" media="screen">
+    {css_style()}
+    </style>
+    </head>
+    <body>
+    """
+
+    html += """<h1 style='text-align:center'>Shopping List</h1>"""
+    html += """<ul>"""
+
+    for item in shopping_list:
+        html += "<li>" + item + "</li>"
+    html += "</ul>"
+    html += "<hr>"
+
+    for recipe in recipes:
+        html += "<div style='break-before:all'>"
+        html += "<hr><hr>"
+        html += f"""<h1 style='text-align:center'> {recipe['Title']}</h1>"""
+        html += "<br>"
+        html += f"""
+        <img 
+            src='{recipe['Image']}'; 
+            alt='{recipe['Title']}'; 
+            class='center';
+        >"""
+
+        html += "</div>"
+
+        ingredient_html = "<h2 style='text-align:center'> Ingredients: </h2>"
+        ingredient_html += "<ul>"
+        for ingredient in recipe['Ingredients']:
+            ingredient_html += "<li>" + ingredient + "</li>"
+        ingredient_html += "</ul>"
+
+        instructions_html = "<h2 style='text-align:center'> Instructions: </h2>"
+        instructions_html += "<p>"+recipe['Instructions']+"</p>"
+        html += f"""
+        <div class="row";>
+            <div class="col left">{ingredient_html}</div>
+            <div class="col right">{instructions_html}</div>
+        </div>
+        """
+
+    html += """
+    </body>
+    </html>
+    """
+    return html
+
+
+def convert_html_to_pdf(source_html, output_filename):
+    # open output file for writing (truncated binary)
+    result_file = open(output_filename, "w+b")
+
+    # convert HTML to PDF
+    pisa_status = pisa.CreatePDF(
+        source_html,                # the HTML to convert
+        dest=result_file)           # file handle to recieve result
+
+    # close output file
+    result_file.close()                 # close output file
+
+    # return False on success and True on errors
+    return pisa_status.err
+
+# d = {
+#         'Title': recipe.title(),
+#         'Time': recipe.total_time(),
+#         'Yield': recipe.yields(),
+#         'Ingredients': recipe.ingredients(),
+#         'Instructions': recipe.instructions(),
+#         'Image': recipe.image(),
+#         'Tags': None
+#     }
+
+
+# This should really be in a separate file:
 known_elements = {
     'flour': sc.Element('Flour',  density=0.7),
     'sugar': sc.Element('Sugar',  density=1.2),
